a62f8641273ea0e23c1505a692fdd9954764e925
henriquel95 <henriquebraayann@gmail.com>
2025-10-07 21:18:22 -0300

    Atualização Settings

--- dolar_agora/settings.py
+++ dolar_agora/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-9od#t6^)8d^d8khj#o5&m4d$q73ggjgk1zdm83-#xjlro37y@6'
+
 import os
 try: 
     SECRET_KEY = os.environ["SECRET_KEY"]
